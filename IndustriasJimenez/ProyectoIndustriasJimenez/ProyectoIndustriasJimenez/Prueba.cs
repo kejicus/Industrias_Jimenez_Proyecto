9db7128712c11d86ac866f9026a3e32f3ad3f230
kejicus <erickazr77@gmail.com>
2013-07-11 19:19:00 -0600

    Proyecto_base

---
+++ IndustriasJimenez/ProyectoIndustriasJimenez/ProyectoIndustriasJimenez/Prueba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoIndustriasJimenez
+{
+    public partial class Prueba : Form
+    {
+        Bienvenido dsd = new Bienvenido();
+        public Prueba()
+        {
+            InitializeComponent();
+            dsd.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
